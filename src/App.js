79e6fd0466813c9c473aa415b655690d260d8c5a
рада <rada@MacBook-Air-Aleksej.local>
2026-03-24 12:57:15 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,51 +1,33 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MainPage from './pages/MainPage/MainPage'
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 import { TicketProvider } from './context/TicketContext';
+import LoadingScreen from './components/LoadingScreen/LoadingScreen';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import MainPage from './pages/MainPage/MainPage';
 import SearchPage from './pages/SearchPage/SearchPage';
 import SeatsSelectionPage from './pages/SeatsSelectionPage/SeatsSelectionPage';
 import PassengersPage from './pages/PassengersPage/PassengersPage';
 import PaymentPage from './pages/PaymentPage/PaymentPage';
 import OrderSuccessPage from './pages/OrderSuccessPage/OrderSuccessPage';
-import LoadingScreen from './components/LoadingScreen/LoadingScreen';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import './App.css';
-
-// Layout компонент для общих элементов
-function Layout() {
-  return (
-    <>
-      <LoadingScreen />
-      <Header />
-      <div className="App">
-        <Outlet /> {/* Дочерние компоненты будут рендериться здесь */}
-      </div>
-      <Footer />
-    </>
-  );
-}
 
 function App() {
-  // Для GitHub Pages нужно указать basename
-  const basename = process.env.NODE_ENV === 'production' 
-    ? '/diplom_1'  // замените на имя вашего репозитория
-    : '/';
-
   return (
     <TicketProvider>
-      <BrowserRouter basename={basename}>
-        <Routes>
-          <Route element={<Layout />}>
+      <BrowserRouter>
+        <LoadingScreen />
+        <Header />
+        <div className="App">
+          <Routes>
             <Route path="/" element={<MainPage />} />
+            <Route path="/" element={<SearchPage />} />
             <Route path="/search" element={<SearchPage />} />
             <Route path="/seats" element={<SeatsSelectionPage />} />
             <Route path="/passengers" element={<PassengersPage />} />
             <Route path="/payment" element={<PaymentPage />} />
             <Route path="/confirmation" element={<OrderSuccessPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Route>
-        </Routes>
+          </Routes>
+          <Footer />
+        </div>
       </BrowserRouter>
     </TicketProvider>
   );
